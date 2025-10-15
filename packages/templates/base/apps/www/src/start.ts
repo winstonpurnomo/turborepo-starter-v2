5ecc3a2cd05f6f6f49927d5f0f847af88a2a8ecc
Winston Purnomo <winstonpurnomo@gmail.com>
2025-10-15 01:44:19 -0700

    chore: remove redundant packages/ folder

--- packages/templates/base/apps/www/src/start.ts
+++
@@ -1,6 +0,0 @@
-import { createStart } from "@tanstack/react-start";
-import { authkitMiddleware } from "@workos/authkit-tanstack-react-start";
-
-export const startInstance = createStart(() => ({
-  requestMiddleware: [authkitMiddleware()],
-}));
--- packages/templates/base/packages/ui/src/hooks/use-toast.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Toast } from "@base-ui-components/react/toast";
-
-export const toastManager = Toast.createToastManager();
-
-export const useToast = Toast.useToastManager;
